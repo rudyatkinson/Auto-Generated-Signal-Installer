b887a0cca791adac61a32c86e387f2709fed712a
bilal <aydnbilal@outlook.com>
2024-04-03 21:56:21 +0300

    Added Settings, Manually Generation and Auto Generation classes.

---
+++ Assets/AGSI/Scripts/Editor/SignalInstallerCodeGenerationManually.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace RudyAtkinson.GenerateCode
+{
+    public class SignalInstallerCodeGenerationManually : EditorWindow
+    {
+        [MenuItem("Tools/RudyAtkinson/Signal Installer Generator/Generate Signal Installer")]
+        public static void GenerateSignalInstaller()
+        {
+            SignalInstallerCodeGeneration.FindSignalsAndGenerateCode();
+        }
+    }
+}
